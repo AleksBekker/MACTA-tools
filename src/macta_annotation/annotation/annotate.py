86bdf156b2a1bca00c62d99fade7802642599171
aleks <alekso.bekker@gmail.com>
2022-04-28 15:58:31 -0400

    Make project back-compatible up to Python 3.7

--- src/macta_annotation/annotation/annotate.py
+++ src/macta_annotation/annotation/annotate.py
@@ -3,34 +3,35 @@
 from anndata import AnnData
 import pandas as pd
 
+from typing import Dict, List, Union
+
 from .tools.cta_tool_interface import CTAToolInterface
 from .tools.celltypist_interface import CelltypistInterface
 
 
 def annotate(
     expr_data: AnnData,
-    ref_data: AnnData,
-    labels_col,
+    ref_data: Union[AnnData, pd.DataFrame],
     annot_type: str,
     result_type: str = 'labels',
-    annot_tools: str | list[str] = '*',
-    tool_interfaces: dict[str, dict[str, CTAToolInterface]] = {
+    annot_tools: Union[str, List[str]] = '*',
+    tool_interfaces: Dict[str, Dict[str, CTAToolInterface]] = {
         'ref': {
             'celltypist': CelltypistInterface(),
         },
         'marker': {},
     },
     **kwargs
-):
+) -> pd.DataFrame:
     """Runs MACTA.
 
     Arguments:
-        expr_data: experimental data on which the analysis is performed
-        ref_data: reference/marker data used to analyse `expr_data`
+        expr_data (AnnData): experimental data on which the analysis is performed
+        ref_data (AnnData/DataFrame): reference/marker data used to analyse `expr_data`
         annot_type (str): type of autoannotation to perform <marker/ref>
         result_type (str): type of results to output <labels>
-        annot_tools (list[str]): tools to use in annotation, '*' to select all tools
-        tool_info: dictionary of `annot_type` -> dict of annotation tool name -> `CTAToolInterface`
+        annot_tools (str/List[str]): tools to use in annotation, '*' to select all tools
+        tool_interfaces (Dict): dictionary of `annot_type` -> dict of annotation tool name -> `CTAToolInterface`
 
     Returns:
         results of auto-annotation
@@ -43,7 +44,7 @@ def annotate(
     return pd.DataFrame(
         {
             tool_name: tool_interfaces[annot_type][tool_name].run_full(
-                expr_data, ref_data, labels_col, result_type
+                expr_data, ref_data, result_type, **kwargs
             )
             for tool_name in annot_tools
         }
