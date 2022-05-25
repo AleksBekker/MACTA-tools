f99dc1a5f45a3010446e2fbd0aee9a9c49d174d3
Aleks Bekker <alekso.bekker@gmail.com>
2022-05-25 16:29:13 -0700

    Fix `macta.utils.is_collection`

--- src/macta/utils/_is_collection.py
+++ src/macta/utils/_is_collection.py
@@ -1,10 +1,9 @@
 
-from collections.abc import Sequence
+from collections.abc import Collection
 
 
 def is_collection(obj) -> bool:
-    '''Checks if an object is a collection. Credit to 
-    https://www.reddit.com/r/learnpython/comments/485h1p/comment/d0hdjef/?utm_source=share&utm_medium=web2x&context=3
+    '''Checks if an object is a collection.
 
     Arguments:
         obj: the object in question
@@ -12,4 +11,4 @@ def is_collection(obj) -> bool:
     Returns:
         `True` if `obj` is a collection, `False` otherwise
     '''
-    return isinstance(obj, Sequence) and not isinstance(obj, (str, bytes, bytearray))
+    return '__contains__' in dir(obj) and not isinstance(obj, (bytearray, bytes, str))
