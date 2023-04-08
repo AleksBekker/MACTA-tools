5feb8b302f0415ba43bd257588fc11c20a58aec3
Aleks Bekker <alekso.bekker@gmail.com>
2023-04-08 01:28:47 -0400

    Fix tox

--- src/macta/utils/__init__.py
+++ src/macta/utils/__init__.py
@@ -1,4 +1,1 @@
 from macta.utils._is_collection import is_collection
-
-from macta.utils._directory_close_mode import DirectoryCloseMode
-from macta.utils._temporary_folder import TemporaryFolder
--- tests/utils/requirements/test_is_instance_requirement.py
+++
@@ -1,3 +0,0 @@
-import pytest
-
-from macta.utils.requirements import IsInstanceRequirement
