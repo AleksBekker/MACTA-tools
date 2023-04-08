63305352b7560f2d4aea04c15a4f3c96ad8db3bb
Aleks Bekker <alekso.bekker@gmail.com>
2023-04-08 00:51:22 -0400

    Remove unneccesary files

--- src/macta/utils/_directory_close_mode.py
+++
@@ -1,9 +0,0 @@
-"""Implementation of an enum for directory close behavior."""
-
-import enum
-
-
-class DirectoryCloseMode(enum.Enum):
-    KEEP_ALL = enum.auto()
-    DELETE_CONTENTS = enum.auto()
-    DELETE_DIRECTORY = enum.auto()
