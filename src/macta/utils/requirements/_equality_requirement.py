438e0beab4784238313b28adbaf296d780975d91
Aleks Bekker <alekso.bekker@gmail.com>
2023-07-26 23:49:42 -0400

    Fix mypy errors in requirement module

--- src/macta/utils/requirements/_equality_requirement.py
+++ src/macta/utils/requirements/_equality_requirement.py
@@ -1,9 +1,9 @@
-from typing import cast
+from typing import Any, cast
 from macta.utils.requirements._requirement import Requirement
 
 
 class EqualityRequirement(Requirement):
     """Class to represent a requirement that a value be exactly equal to a specific value."""
 
-    def _checker(self, other_value) -> bool:
+    def _checker(self, other_value: Any) -> bool:
         return cast(bool, self.value == other_value)
