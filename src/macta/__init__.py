d8859c4aabefdae6cd657e38b189e1c622a83162
Aleks Bekker <alekso.bekker@gmail.com>
2023-08-04 13:52:14 -0400

    Try removing the specific tool imports
    I suspect the error suppression might be interfering with something in
    the GitHub actions.

--- src/macta/__init__.py
+++ src/macta/__init__.py
@@ -1,6 +1,4 @@
-# isort: skip_file
-from macta import utils
-from macta import tools
+from macta import tools, utils
 from macta._annotate import annotate
 
 __all__ = ['annotate', 'tools', 'utils']
